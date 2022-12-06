e8c4fb7d9a2b655b7a749f89934b7dfef5c6abdb
Dmitry Butyugin <dmbutyugin@google.com>
2022-12-06 22:20:30 +0100

    extruder: PA velocity squared term support
    Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

--- klippy/chelper/kin_extruder.c
+++ klippy/chelper/kin_extruder.c
@@ -18,7 +18,8 @@
 // into the extruder during acceleration (and retracted during
 // deceleration). The formula is:
 //     pa_position(t) = (nominal_position(t)
-//                       + pressure_advance * nominal_velocity(t))
+//                       + advance * nominal_velocity(t)
+//                       [ + advance2 * nominal_velocity(t)^2])
 // Which is then "smoothed" using a weighted average:
 //     smooth_position(t) = (
 //         definitive_integral(pa_position(x) * (smooth_time/2 - abs(t-x)) * dx,
@@ -52,21 +53,43 @@ extruder_integrate_time(double base, double start_v, double half_accel
 
 // Calculate the definitive integral of extruder for a given move
 static double
-pa_move_integrate(struct move *m, double pressure_advance
-                  , double base, double start, double end, double time_offset)
+pa_move_integrate(struct move *m, double linear_velocity, double linear_offset
+                  , double linear_advance, double base, double start, double end
+                  , double time_offset)
 {
     if (start < 0.)
         start = 0.;
     if (end > m->move_t)
         end = m->move_t;
+    double start_v = m->start_v;
+    double ha = m->half_accel;
     // Calculate base position and velocity with pressure advance
     int can_pressure_advance = m->axes_r.y != 0.;
-    if (!can_pressure_advance)
-        pressure_advance = 0.;
-    base += pressure_advance * m->start_v;
-    double start_v = m->start_v + pressure_advance * 2. * m->half_accel;
+    if (can_pressure_advance) {
+        double switch_t =
+            !ha || !linear_velocity ?  0.
+                                    : .5 * (linear_velocity - start_v) / ha;
+        if (switch_t > start && switch_t < end) {
+            double res = pa_move_integrate(
+                    m, linear_velocity, linear_offset, linear_advance,
+                    base, start, switch_t, time_offset);
+            res += pa_move_integrate(
+                    m, linear_velocity, linear_offset, linear_advance,
+                    base, switch_t, end, time_offset);
+            return res;
+        } else if (start_v + ha * (start + end) < linear_velocity) {
+            double recip_lv = 1. / linear_velocity;
+            base += (linear_advance
+                    + linear_offset * recip_lv * (2. - recip_lv * start_v)) * start_v;
+            start_v += (linear_advance
+                        + 2. * linear_offset * recip_lv * (1. - recip_lv * start_v)) * 2. * ha;
+            ha -= 4. * linear_offset * recip_lv * recip_lv * ha * ha;
+        } else {
+            base += linear_offset + linear_advance * start_v;
+            start_v += 2. * linear_advance * ha;
+        }
+    }
     // Calculate definitive integral
-    double ha = m->half_accel;
     double iext = extruder_integrate(base, start_v, ha, start, end);
     double wgt_ext = extruder_integrate_time(base, start_v, ha, start, end);
     return wgt_ext - time_offset * iext;
@@ -74,36 +97,42 @@ pa_move_integrate(struct move *m, double pressure_advance
 
 // Calculate the definitive integral of the extruder over a range of moves
 static double
-pa_range_integrate(struct move *m, double move_time
-                   , double pressure_advance, double hst)
+pa_range_integrate(struct move *m, double move_time, double linear_velocity
+                   , double linear_offset, double linear_advance, double hst)
 {
     // Calculate integral for the current move
     double res = 0., start = move_time - hst, end = move_time + hst;
     double start_base = m->start_pos.x;
-    res += pa_move_integrate(m, pressure_advance, 0., start, move_time, start);
-    res -= pa_move_integrate(m, pressure_advance, 0., move_time, end, end);
+    res += pa_move_integrate(m, linear_velocity, linear_offset, linear_advance,
+                             0., start, move_time, start);
+    res -= pa_move_integrate(m, linear_velocity, linear_offset, linear_advance,
+                             0., move_time, end, end);
     // Integrate over previous moves
     struct move *prev = m;
     while (unlikely(start < 0.)) {
         prev = list_prev_entry(prev, node);
         start += prev->move_t;
         double base = prev->start_pos.x - start_base;
-        res += pa_move_integrate(prev, pressure_advance, base, start
-                                 , prev->move_t, start);
+        res += pa_move_integrate(
+                prev, linear_velocity, linear_offset, linear_advance,
+                base, start, prev->move_t, start);
     }
     // Integrate over future moves
     while (unlikely(end > m->move_t)) {
         end -= m->move_t;
         m = list_next_entry(m, node);
         double base = m->start_pos.x - start_base;
-        res -= pa_move_integrate(m, pressure_advance, base, 0., end, end);
+        res -= pa_move_integrate(
+                m, linear_velocity, linear_offset, linear_advance,
+                base, 0., end, end);
     }
     return res;
 }
 
 struct extruder_stepper {
     struct stepper_kinematics sk;
-    double pressure_advance, half_smooth_time, inv_half_smooth_time2;
+    double linear_velocity, linear_offset, linear_advance;
+    double half_smooth_time, inv_half_smooth_time2;
 };
 
 static double
@@ -116,13 +145,16 @@ extruder_calc_position(struct stepper_kinematics *sk, struct move *m
         // Pressure advance not enabled
         return m->start_pos.x + move_get_distance(m, move_time);
     // Apply pressure advance and average over smooth_time
-    double area = pa_range_integrate(m, move_time, es->pressure_advance, hst);
+    double area = pa_range_integrate(
+            m, move_time, es->linear_velocity, es->linear_offset,
+            es->linear_advance, hst);
     return m->start_pos.x + area * es->inv_half_smooth_time2;
 }
 
 void __visible
 extruder_set_pressure_advance(struct stepper_kinematics *sk
-                              , double pressure_advance, double smooth_time)
+                              , double linear_velocity, double linear_offset
+                              , double linear_advance, double smooth_time)
 {
     struct extruder_stepper *es = container_of(sk, struct extruder_stepper, sk);
     double hst = smooth_time * .5;
@@ -131,7 +163,9 @@ extruder_set_pressure_advance(struct stepper_kinematics *sk
     if (! hst)
         return;
     es->inv_half_smooth_time2 = 1. / (hst * hst);
-    es->pressure_advance = pressure_advance;
+    es->linear_velocity = linear_velocity;
+    es->linear_offset = linear_offset;
+    es->linear_advance = linear_advance;
 }
 
 struct stepper_kinematics * __visible
--- klippy/kinematics/extruder.py
+++ klippy/kinematics/extruder.py
@@ -11,7 +11,13 @@ class ExtruderStepper:
         self.printer = config.get_printer()
         self.name = config.get_name().split()[-1]
         self.pressure_advance = self.pressure_advance_smooth_time = 0.
+        self.linear_velocity = self.linear_offset = 0.
         self.config_pa = config.getfloat('pressure_advance', 0., minval=0.)
+        self.config_pa_lin_offset = config.getfloat(
+                'pressure_advance_linear_offset', 0., minval=0.)
+        self.config_pa_lin_vel = config.getfloat(
+                'pressure_advance_linear_velocity', 0., minval=0.,
+                above=(0. if self.config_pa_lin_offset else -1.))
         self.config_smooth_time = config.getfloat(
                 'pressure_advance_smooth_time', 0.040, above=0., maxval=.200)
         # Setup stepper
@@ -46,9 +52,13 @@ class ExtruderStepper:
     def _handle_connect(self):
         toolhead = self.printer.lookup_object('toolhead')
         toolhead.register_step_generator(self.stepper.generate_steps)
-        self._set_pressure_advance(self.config_pa, self.config_smooth_time)
+        self._set_pressure_advance(self.config_pa, self.config_pa_lin_vel,
+                                   self.config_pa_lin_offset,
+                                   self.config_smooth_time)
     def get_status(self, eventtime):
         return {'pressure_advance': self.pressure_advance,
+                'linear_velocity': self.linear_velocity,
+                'linear_offset': self.linear_offset,
                 'smooth_time': self.pressure_advance_smooth_time}
     def find_past_position(self, print_time):
         mcu_pos = self.stepper.get_past_mcu_position(print_time)
@@ -65,20 +75,24 @@ class ExtruderStepper:
                                              % (extruder_name,))
         self.stepper.set_position([extruder.last_position, 0., 0.])
         self.stepper.set_trapq(extruder.get_trapq())
-    def _set_pressure_advance(self, pressure_advance, smooth_time):
+    def _set_pressure_advance(self, pressure_advance, linear_velocity,
+                              linear_offset, smooth_time):
         old_smooth_time = self.pressure_advance_smooth_time
-        if not self.pressure_advance:
+        if not self.pressure_advance and not self.linear_offset:
             old_smooth_time = 0.
         new_smooth_time = smooth_time
-        if not pressure_advance:
+        if not pressure_advance and not linear_offset:
             new_smooth_time = 0.
         toolhead = self.printer.lookup_object("toolhead")
         toolhead.note_step_generation_scan_time(new_smooth_time * .5,
                                                 old_delay=old_smooth_time * .5)
         ffi_main, ffi_lib = chelper.get_ffi()
         espa = ffi_lib.extruder_set_pressure_advance
-        espa(self.sk_extruder, pressure_advance, new_smooth_time)
+        espa(self.sk_extruder, linear_velocity, linear_offset, pressure_advance,
+             new_smooth_time)
         self.pressure_advance = pressure_advance
+        self.linear_velocity = linear_velocity
+        self.linear_offset = linear_offset
         self.pressure_advance_smooth_time = smooth_time
     cmd_SET_PRESSURE_ADVANCE_help = "Set pressure advance parameters"
     def cmd_default_SET_PRESSURE_ADVANCE(self, gcmd):
@@ -92,13 +106,22 @@ class ExtruderStepper:
     def cmd_SET_PRESSURE_ADVANCE(self, gcmd):
         pressure_advance = gcmd.get_float('ADVANCE', self.pressure_advance,
                                           minval=0.)
+        linear_offset = gcmd.get_float('LINEAR_OFFSET',
+                                       self.linear_offset, minval=0.)
+        linear_velocity = gcmd.get_float('LINEAR_VELOCITY',
+                                         self.linear_velocity, minval=0.,
+                                         above=(0. if linear_offset else -1.))
         smooth_time = gcmd.get_float('SMOOTH_TIME',
                                      self.pressure_advance_smooth_time,
                                      minval=0., maxval=.200)
-        self._set_pressure_advance(pressure_advance, smooth_time)
+        self._set_pressure_advance(pressure_advance, linear_velocity,
+                                   linear_offset, smooth_time)
         msg = ("pressure_advance: %.6f\n"
+               "linear_offset: %.6f\n"
+               "linear_velocity: %.6f\n"
                "pressure_advance_smooth_time: %.6f"
-               % (pressure_advance, smooth_time))
+               % (pressure_advance, linear_offset, linear_velocity,
+                   smooth_time))
         self.printer.set_rollover_info(self.name, "%s: %s" % (self.name, msg))
         gcmd.respond_info(msg, log=False)
     cmd_SET_E_ROTATION_DISTANCE_help = "Set extruder rotation distance"
